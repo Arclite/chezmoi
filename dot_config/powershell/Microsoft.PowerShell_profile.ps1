4a15f7ba72530cd1a0c61ca09979d85a199c7072
Geoff Pado <geoff@pado.name>
2022-04-28 03:48:09 -0700

    Add Powershell ls alias instead of using installed ls

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -13,6 +13,8 @@ function title {
     $Host.UI.RawUI.WindowTitle = $title
 }
 
+New-Alias ls Get-ChildItem
+
 . "$HOME/.config/powershell/LocalSettings.ps1" | Invoke-Expression
 
 oh-my-posh init pwsh --config ~/.config/powershell/cocoatype.omp.json | Invoke-Expression
