88b126c59f2e9dfc6d77b72d9a477c1e0f2374a5
Geoff Pado <geoff@pado.name>
2026-03-26 20:32:20 -0700

    Remove selected Emacs packages

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -137,8 +137,7 @@
  '(flymake-luac-program "/Users/pado/.asdf/installs/lua/5.4.3/bin/luac")
  '(org-agenda-files '("~/Development/iOS/PlaygroundShare/shareplay.org"))
  '(org-startup-truncated nil)
- '(package-selected-packages
-   '(jq-mode restclient-helm restclient-jq rg dash-at-point graphql-ts-mode kotlin-mode typescript-mode tree-sitter-langs bazel helm-projectile helm eglot powershell go-mode docker flycheck flymake-quickdef flymake-lua lua-mode counsel-jq json-mode docker-compose-mode magit dash dracula-theme ac-cider cider yaxception yaml-mode web-mode tagedit swift-mode spinner sourcekit smex seq restclient relative-line-numbers rainbow-delimiters queue projectile paredit org markdown-mode log4e js2-mode javap-mode inf-ruby ido-ubiquitous exec-path-from-shell elm-mode dockerfile-mode company color-theme clojure-mode-extra-font-locking base16-theme android-mode adaptive-wrap))
+ '(package-selected-packages nil)
  '(swift-mode:parenthesized-expression-offset 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
