379ab4da05fcacf8d51a876bce6be2ae4084e739
Geoff Pado <gpado@wandb.com>
2026-03-30 09:43:17 -0700

    Add Emacs agent-shell package

--- dot_emacs.d/customizations/llm.el
+++ dot_emacs.d/customizations/llm.el
@@ -7,3 +7,13 @@
         gptel-backend (gptel-make-anthropic "Claude"
                         :stream t
                         :key gptel-api-key)))
+
+;; Agent Shell for communicating with Claude Code
+
+(use-package agent-shell
+    :ensure t
+    :ensure-system-package
+    ((claude . "curl -fsSL https://claude.ai/install.sh | bash")
+     (claude-agent-acp . "npm i -g @agentclientprotocol/claude-agent-acp"))
+    :config
+    (setq agent-shell-header-style nil))
