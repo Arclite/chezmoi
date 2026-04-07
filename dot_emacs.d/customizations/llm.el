b4582e266384c418d832b82e45d89adcb069183f
Geoff Pado <gpado@wandb.com>
2026-04-06 18:34:50 -0700

    Prevent copying current line into agent-shell

--- dot_emacs.d/customizations/llm.el
+++ dot_emacs.d/customizations/llm.el
@@ -15,6 +15,7 @@
     :ensure-system-package ((claude . "curl -fsSL https://claude.ai/install.sh | bash")
                             (claude-agent-acp . "npm i -g @agentclientprotocol/claude-agent-acp"))
     :config
-    (setq agent-shell-header-style nil)
-    (setq agent-shell-preferred-agent-config (agent-shell-anthropic-make-claude-code-config))
-    (setq agent-shell-anthropic-default-session-mode-id "plan"))
+    (setq agent-shell-header-style nil
+          agent-shell-preferred-agent-config (agent-shell-anthropic-make-claude-code-config)
+          agent-shell-anthropic-default-session-mode-id "plan"
+          agent-shell-context-sources '(files region error)))
