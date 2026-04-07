7870cdf4e2dd1a00b1cae61ca02417e7ba35a5b5
Geoff Pado <gpado@wandb.com>
2026-04-06 18:47:58 -0700

    Improve Swift file editing

--- dot_emacs.d/customizations/swift.el
+++ dot_emacs.d/customizations/swift.el
@@ -3,6 +3,9 @@
        :branch "main")
   :ensure t
   :mode "\\.swift\\'"
+  :custom ((swift-ts-basic-offset 4)
+           (swift-ts-switch-case-offset 0)
+           (swift-ts:align-multiline-string nil))
   :hook ((swift-ts-mode . eglot-ensure)
          (swift-ts-mode . company-mode)))
 
@@ -14,3 +17,23 @@
 
 (add-to-list 'compilation-error-regexp-alist 'xcbeautify-error)
 (add-to-list 'compilation-error-regexp-alist 'xcbeautify-warning)
+
+;; Auto-insert header
+(defvar my/copyright-owner "Geoff Pado"
+  "Name to use in copyright headers. Can be set via dir-locals.")
+(put 'my/copyright-owner 'safe-local-variable #'stringp)
+
+(use-package autoinsert
+  :ensure nil ;; built-in package
+  :init
+  (auto-insert-mode 1)
+  :config
+  (setq auto-insert-query nil)
+  (define-auto-insert '("\\.swift\\'" . "Swift header")
+    '(
+      "Short description: "
+      "//  Copyright © "
+      (format-time-string "%Y")
+      " "
+      my/copyright-owner
+      ". All rights reserved." \n \n _ \n)))
