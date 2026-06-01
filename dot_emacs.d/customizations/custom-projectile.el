d6ea251127be162c62b1ef34a6bc2af55e88e2fc
Geoff Pado <gpado@wandb.com>
2026-06-01 14:45:08 -0700

    Change default project search command

--- dot_emacs.d/customizations/custom-projectile.el
+++ dot_emacs.d/customizations/custom-projectile.el
@@ -1,12 +1,18 @@
 ;; Projectile customizations
 
+(use-package rg
+  :init
+  (rg-enable-default-bindings)
+  :ensure t)
+
 (use-package projectile
   :init
   (projectile-mode +1)
   :bind-keymap
   ("s-p" . projectile-command-map)
   :bind (:map projectile-command-map
-              ("p" . helm-projectile-switch-project))
+              ("p" . helm-projectile-switch-project)
+              ("s r" . rg-project))
   :custom
   (projectile-indexing-method 'hybrid) ;; Use hybrid indexing method
   (projectile-switch-project-action 'projectile-vc)
